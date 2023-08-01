436fab5c00b69b8765095f6dcfadcc958acd4bba
Muhammad Rehan Ul Haq <137270862+muhammadrehan463@users.noreply.github.com>
2023-08-01 12:00:23 -0400

    firstc commit

---
+++ src/main/java/auth2/example/authorizationserver/AuthorizationServerApplication.java
@@ -0,0 +1,14 @@
+package auth2.example.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+
+}
